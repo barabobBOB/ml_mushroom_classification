81ed3b124e3cc3a49603c536621591f1c291cb45
osm1892 <osm189214@gmail.com>
2022-10-08 12:45:28 +0000

    wip

--- classification.py
+++ classification.py
@@ -1,4 +1,5 @@
 import cuml
+from cuml.preprocessing import LabelEncoder, OneHotEncoder
 import numpy as np
 import cudf
 from typing import Tuple
@@ -27,13 +28,16 @@ def load_data_from_csv(csv_path: str) -> Tuple[np.ndarray, np.ndarray]:
     Returns:
         Tuple[np.ndarray, np.ndarray]: array 형태의 X, Y 값입니다.
     """
-    data: cudf.DataFrame = cudf.read_csv(csv_path)
+    data: cudf.DataFrame = cudf.read_csv("mushrooms.csv")
+    data.drop("veil-type", axis=1, inplace=True)  # 종류가 하나밖에 없음.
 
-    x: cudf.DataFrame = data.drop("class", axis=1)
-    y: cudf.Series = data["class"]
+    label_encoder = LabelEncoder()
+    onehot_encoder = OneHotEncoder()
 
-    label_encoder = cuml.preprocessing.LabelEncoder()
-    onehot = cuml.preprocessing.OneHotEncoder()
+    x: cudf.DataFrame = cudf.get_dummies(data.drop("class", axis=1))
+    y: cudf.Series = label_encoder.fit_transform(data["class"])
+
+    return x, y
 
 
 def split_data(
@@ -43,10 +47,10 @@ def split_data(
     데이터를 train, test 데이터로 나눕니다.
 
     Args:
-        x (np.ndarray): _description_
-        y (np.ndarray): _description_
-        test_ratio (float): _description_
-        random_state (int): _description_
+        x (np.ndarray): 데이터
+        y (np.ndarray): 레이블
+        test_ratio (float): 테스트셋의 비율(0~1)
+        random_state (int): 셔플에 사용할 랜덤 시드
 
     Returns:
         Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
