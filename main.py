e078358737d060c3c375b0e071f9aa288c4d2e58
choiseyeon <choi20014830@gmail.com>
2022-10-11 01:04:22 +0900

    feat: 각 모델 정확도 시각화

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import classification
+import matplotlib.pyplot as plt
 
 x, y = classification.load_data_from_csv("mushrooms.csv")
 
@@ -6,7 +7,14 @@ train_x, test_x, train_y, test_y = classification.split_data(
     x, y, 0.2, classification.RANDOM_SEED
 )
 
+modeling_score = {}
+
 for name in classification.classifier_names:
     classifier = classification.classifiers[name]
     classifier.fit(train_x, train_y)
+    modeling_score[name] = classifier.score(test_x, test_y)*100
     print(f"{name} score: {classifier.score(test_x, test_y)}")
+
+score_frame: classification.pd.Series = classification.pd.Series(modeling_score, name="score")
+score_frame.plot.bar(color=['r', 'c', 'g', 'b'])
+plt.show()
